686f5a4962d77bce927d0236a1dda9ec273cd72a
Rajendar Singh <rajendarsingh@Laptop334.local>
2025-05-22 22:46:03 +0530

    18

---
+++ AY/Intermediate/18/main.py
@@ -0,0 +1,88 @@
+import turtle as t
+import random
+# import colorgram as cg
+# tim = t.Turtle()
+# t.colormode(255)
+
+# rgb_colors = []
+# colors = cg.extract("image.jpeg", 36)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     rgb_colors.append((r, g, b))
+
+# print(rgb_colors)
+# tim.shape("turtle")
+# tim.color("red")
+
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     return (r, g, b)
+
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+
+# for shape_side_n in range(3, 10):
+#     tim.color(colours[shape_side_n-3])
+#     draw_shape(shape_side_n)
+
+# tim.pensize(15)
+# tim.speed("fastest")
+
+# def draw_spirograph(size_of_gap):
+#     for _ in range(int(360 / size_of_gap)):
+#         tim.color(random_color())
+#         tim.circle(100)
+#         tim.setheading(tim.heading() + size_of_gap)
+
+# draw_spirograph(5)
+
+# directions = [0, 90, 180, 270]
+
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+
+tim = t.Turtle()
+t.colormode(255)    
+color_list = [(211, 210, 210), (189, 167, 121), (57, 90, 111), (113, 43, 35), (163, 89, 64), (210, 212, 214), (208, 211, 208), (211, 209, 210), (64, 43, 43), (171, 183, 170), (136, 149, 69), (127, 160, 172), (101, 79, 89), (83, 133, 108), (108, 39, 44), (39, 61, 47), (45, 40, 41), (211, 196, 124), (174, 150, 152), (36, 71, 88), (179, 106, 80), (36, 67, 84), (207, 185, 181), (99, 140, 119), (184, 198, 181), (148, 116, 120), (204, 183, 186), (180, 195, 200), (53, 69, 59), (122, 129, 135), (113, 132, 138), (181, 193, 205)]
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))  
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+
+
+
+
+
+my_screen = t.Screen()
+my_screen.exitonclick()
