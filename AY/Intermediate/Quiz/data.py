d2d939e734abdc495eb95f0a36f5a13cba44120c
Rajendar Singh <rajendarsingh@Laptop334.local>
2025-05-22 20:56:11 +0530

    quiz done

---
+++ AY/Intermediate/Quiz/data.py
@@ -0,0 +1,102 @@
+question_data = [
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Jingle Bells was originally meant for Thanksgiving",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "The Lego Group was founded in 1932.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Is a dog a mammal?",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "French is an official language in Canada.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Adolf Hitler was born in Australia. ",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "The Sun rises from the North.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "The color orange is named after the fruit.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "The National Animal of Scotland is the Unicorn.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, and artists who died at the age of 27.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+}
+]
