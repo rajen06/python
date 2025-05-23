89f7c69a7c94b85c3ae4b3d0826cbce17f92505d
Rajendar Singh <rajendarsingh@Laptop334.local>
2025-05-23 14:49:04 +0530

    pong game

---
+++ AY/Intermediate/22/main.py
@@ -0,0 +1,54 @@
+from turtle import Screen, Turtle
+import time
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+screen = Screen()
+screen.setup(width=800 , height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((380, 0))
+l_paddle = Paddle((-380, 0))
+
+ball = Ball()
+
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(ball.move_speed)
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+screen.exitonclick()
+
+
